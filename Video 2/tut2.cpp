efcb5de970c59e3af547f3c3bb2f57b5583c223e
itsaivivek <er.vivek.social@gmail.com>
2025-06-18 12:25:47 +0545

    Basic Structure of C++ Program

---
+++ Video 2/tut2.cpp
@@ -0,0 +1,7 @@
+#include<iostream> // Header file : iostream is used for input ouput task
+
+using namespace std; // gives information about the function used in program like cout
+int main(){ // main is a entry point of program from where it start
+    cout<<"Hello World"; // cout is used for displaying.
+    return 0; // return 0 to operating system which called main() shows that it is successfully executed.
+}
