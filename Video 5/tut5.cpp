5c476fbac77b07879da02b5ef65a434a73367585
unknown <er.vivek.social@gmail.com>
2025-08-16 10:42:10 +0545

    C++ Basic Input/Output & More

---
+++ Video 5/tut5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // << this is called insertion operator
+    // >> this is called extraction operator
+    int num1, num2;
+    cout<<"Enter the value of num1:\n";
+    cin>>num1;
+    cout<<"Enter the value of num2:\n";
+    cin>>num2;
+
+    cout<<"The sum is "<< num1+num2;
+    return 0;
+}
