0d3da373a1535ef44865fb25f276ed9c27e32c2d
unknown <er.vivek.social@gmail.com>
2025-08-31 15:56:36 +0545

    Solution 3

---
+++ Practise Questions/Solutions/03_problem3.cpp
@@ -0,0 +1,14 @@
+// Declare variables in C++ following naming rules and print their values.
+#include<iostream>
+using namespace std;
+
+int main(){
+    int id = 1;
+    float salaryInDollor = 42.5;
+    double Yearly_salary = 123.456;
+    char favLetter = 'v';
+    bool myBoolean1 = true;
+    string name = "Vivek";
+    cout<<id<<endl<<salaryInDollor<<endl<<Yearly_salary<<endl<<favLetter<<endl<<myBoolean1<<endl<<name<<endl;
+    return 0;
+}
