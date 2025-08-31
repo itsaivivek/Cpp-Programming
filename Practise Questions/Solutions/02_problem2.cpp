c9232c21937172beb0fafa1290920f2158759134
unknown <er.vivek.social@gmail.com>
2025-08-31 15:43:40 +0545

    Solution 2

---
+++ Practise Questions/Solutions/02_problem2.cpp
@@ -0,0 +1,8 @@
+// Create a basic C++ program that includes the main function and outputs a simple string.
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"This is a String"<<endl;
+    return 0;
+}
