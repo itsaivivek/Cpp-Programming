8e6c992f5f97bfcfb62122e0893df04402fe3887
unknown <er.vivek.social@gmail.com>
2025-08-31 15:40:12 +0545

    Solution 1

---
+++ Practise Questions/Solutions/01_problem1.cpp
@@ -0,0 +1,8 @@
+// 1. Write a C++ program to print a welcome message after setting up your development environment with VS Code and g++.
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Welcome to VS Code"<<endl;
+    return 0;
+}
