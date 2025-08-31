f453ca27ed27b420930ac237ac717e6260a13e9b
unknown <er.vivek.social@gmail.com>
2025-08-31 16:00:03 +0545

    Solution 4

---
+++ Practise Questions/Solutions/04_problem4.cpp
@@ -0,0 +1,12 @@
+// Write a program demonstrating the use of primitive data types like int, float, char, and bool.
+#include<iostream>
+using namespace std;
+
+int main(){
+    int rollNumber = 32;
+    float id = 456.45;
+    char letter = 'd';
+    bool value = false;
+    cout<<rollNumber<<endl<<id<<endl<<letter<<endl<<value<<endl;
+    return 0;
+}
