8d2ff640402b3be7a0e10d9402180646ad1df58b
itsaivivek <er.vivek.social@gmail.com>
2025-09-17 11:31:19 +0545

    Solution to Exercise on Cpp Inheritance

---
+++ Video 47/tut47.cpp
@@ -0,0 +1,63 @@
+/*
+Create 2 classes:
+    1. SimpleCalculator - Takes input of 2 number using utility function and performs +, -, *, / and displays the results another function.
+    2. ScientificCalculator - Takes input of 2 number using utility function performs any four scientifc operation of your choice and displays the results another function.
+
+Create another class HybridCalculator and inherit it using these 2 classes:
+Q1. What type of Inheritance are you using? 
+---> Multiple Inheritance
+Q2. Which mode of Inheritance are you using?
+--->  public SimpleCalculator, public ScientificCalculator
+Q3. Create an object of HybridCalculator and display results of simple and scientific calulator.
+Q3. How is code reusability implemented?
+*/
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class SimpleCalculator{
+    int a,b;
+    public:
+        void getDataSimple(){
+            cout<<"Enter the value of a"<<endl;
+            cin>>a;
+            cout<<"Enter the value of b"<<endl;
+            cin>>b;
+        }
+        void performOperationsSimple(){
+            cout<<"The value of a + b is: "<<(a+b)<<endl;
+            cout<<"The value of a - b is: "<<(a-b)<<endl;
+            cout<<"The value of a * b is: "<<(a*b)<<endl;
+            cout<<"The value of a / b is: "<<(a/b)<<endl;
+        }
+};
+
+class ScientificCalculator{
+    int a,b;
+    public:
+        void getDataScientific(){
+            cout<<"Enter the value of a"<<endl;
+            cin>>a;
+            cout<<"Enter the value of b"<<endl;
+            cin>>b;
+        }
+        void performOperationsScientific(){
+            cout<<"The value of cos(a) is: "<<cos(a)<<endl;
+            cout<<"The value of sin(a) is: "<<sin(a)<<endl;
+            cout<<"The value of exp(a) is: "<<exp(a)<<endl;
+            cout<<"The value of tan(a) is: "<<tan(a)<<endl;
+        }
+};
+
+class HybridCalculator: public SimpleCalculator, public ScientificCalculator{
+
+};
+
+int main(){
+    HybridCalculator calc;
+    calc.getDataScientific();
+    calc.performOperationsScientific();
+    calc.getDataSimple();
+    calc.performOperationsSimple();
+    return 0;
+}
