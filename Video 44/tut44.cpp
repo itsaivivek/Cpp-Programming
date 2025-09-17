6bc78e4c63a76f843c7676294b4d7d63dd9ca43e
itsaivivek <er.vivek.social@gmail.com>
2025-09-17 09:34:15 +0545

    Virtual Base Class in C++

---
+++ Video 44/tut44.cpp
@@ -0,0 +1,39 @@
+/*
+Consider the situation where we have one class A . This class A is inherited by two other classes B and C. Both these class are inherited
+into another in a new class D. As we can see from the figure that data members/function of class A are inherited twice to class D. One 
+through class B and second through class C. When any data / function member of class A is accessed by an object of class D, ambiguity arises 
+as to which data/function member would be called? One inherited through B or the other inherited through C. This confuses compiler and 
+it displays error.
+
+Note: 
+virtual can be written before or after the public. Now only one copy of data/function member will be copied to class C and class B and class A
+ becomes the virtual base class. Virtual base classes offer a way to save space and avoid ambiguities in class hierarchies that use multiple
+  inheritances. When a base class is specified as a virtual base, it can act as an indirect base more than once without duplication of its 
+  data members. A single copy of its data members is shared by all the base classes that use virtual base. 
+*/
+
+#include<iostream>
+using namespace std;
+
+class A{
+    public:
+        int a;
+        A(){
+            a = 10;
+        }
+};
+class B: virtual public A{
+
+};
+class C: virtual public A{
+
+};
+class D: public B, public C{
+
+};
+
+int main(){
+    D obj;
+    cout<<"a = "<<obj.a<<endl;
+    return 0;
+}
