778011b540959781f01d13cc229f5fa486534f23
itsaivivek <er.vivek.social@gmail.com>
2025-10-24 11:30:57 +0545

    Initialization list in Constructure in C++

---
+++ Video 49/tut49.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+/*
+Syntax for initialization list in constructor:
+constructor(argument-list) : initialization-section
+{
+    assignment + other code;
+}
+
+class Test
+{
+    int a;
+    int b;
+    public:
+        Test(int i, int j) : a(i), b(j)
+        {
+            constructor-body
+        }
+};
+
+*/
+
+class Test
+{
+    int a;
+    int b;
+
+public:
+    // Test(int i, int j) : a(i), b(j)
+    // Test(int i, int j) : a(i), b(i + j)
+    // Test(int i, int j) : a(i), b(2 * j)
+    // Test(int i, int j) : a(i), b(a + j)
+    // Test(int i, int j) : b(j), a(i + b) // --> RED Flag this will create problem because a is will be initialized first because a is declared first in Test class
+    Test(int i, int j): a(i)
+    {
+        b = j;
+        cout << "Contructor executed" << endl;
+        cout << "Value of a is " << a << endl;
+        cout << "Value of b is " << b << endl;
+    }
+};
+
+int main()
+{
+    Test t(4, 6);
+
+    return 0;
+}
