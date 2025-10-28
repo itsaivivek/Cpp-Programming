3ec1ae7373144bf0eb4b6b8e2e6db72908b7be7d
itsaivivek <er.vivek.social@gmail.com>
2025-10-28 11:16:24 +0545

    Polymorphism in C++

---
+++ Video 54/tut54.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+/*
+Polymorphism
+    - one name and multiple forms
+    - eg. Function overloading, operator overloading, virtual function 
+
+Types of Polymorphism
+    1. Compile Time Polymorphism:
+         (i) Function Overloading
+        (ii) Operator Overloading
+    
+    2. Run Time Polymorphism:
+         (i) Virtual Functions
+    
+*/
+    return 0;
+}
