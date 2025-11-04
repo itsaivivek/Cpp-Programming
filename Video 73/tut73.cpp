6bd2912cc35121219299cddd6d77dd10e2ecebbf
itsaivivek <er.vivek.social@gmail.com>
2025-11-04 19:20:33 +0545

    Map in C++ STL

---
+++ Video 73/tut73.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<map>
+#include<string>
+using namespace std;
+
+// Map is an associative array
+
+int main(){
+    map<string, int> marksMap;
+    marksMap["Harry"] = 98;
+    marksMap["Jack"] = 59;
+    marksMap["Rohan"] = 2;
+
+    marksMap.insert({{"Vivek", 169}, {"Ankit", 187}});
+    map<string, int> :: iterator iter;
+    for(iter = marksMap.begin(); iter != marksMap.end(); iter++){
+        cout<<(*iter).first<<" "<<(*iter).second<<"\n";
+    }
+    cout<<"The size is: "<<marksMap.size()<<endl;
+    cout<<"The max size is: "<<marksMap.max_size()<<endl;
+    cout<<"The empty's return value is: "<<marksMap.empty()<<endl;
+
+    return 0;
+}
